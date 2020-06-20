b2ef3f3e11faad4c8cd7a9f15fd116c3239a260c
edumorris <eduuwachira@gmail.com>
2020-06-20 21:59:41 +0300

    Base code variables initialized

--- src/app/gitter/gitter.component.ts
+++ src/app/gitter/gitter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-gitter',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./gitter.component.css']
 })
 export class GitterComponent implements OnInit {
+  searchParam: string;
+  searchData = [];
+  searchUrl = "https://api.github.com/users/";
+  token = "a84c1f0a1f6f01f6fac34e35f8ea92eab81845a1";
+  searcher = this.searchUrl + this.searchParam + "?access_token=" + this.token;
+  user: string;
+  description: string;
 
-  constructor() { }
+submitSearch() {
+  
+ }
 
-  ngOnInit(): void {
-  }
+constructor(private http: HttpClient) {
+  this.http.get(this.searcher).toPromise().then(data => {
+    for (let key in data) {
+      if (data.hasOwnProperty(key)) {
+        this.searchData.push(data[key]);
+      }
+    }
+  });
+
+ }
+
+ngOnInit(): void {
+}
 
 }
