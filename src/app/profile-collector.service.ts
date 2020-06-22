d749378aa3cd2ef4ade7b1e2e856db206c028598
edumorris <eduuwachira@gmail.com>
2020-06-22 13:10:38 +0300

    Build-Access token fix

--- src/app/profile-collector.service.ts
+++ src/app/profile-collector.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {environment } from '../environments/environment';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +10,11 @@ export class ProfileCollectorService {
   private searchParam: string;
   private startUrl = "https://api.github.com/users/";
   private repoAdd = "/repos?access_token=";
-  
+  private aTokken = "a84c1f0a1f6f01f6fac34e35f8ea92eab81845a1";
+
   constructor(private http: HttpClient) {
     this.searchParam = "edumorris";
-   }
+  }
 
   getProfile(searchParam: string) {
     return this.searchParam = searchParam;
@@ -21,14 +22,22 @@ export class ProfileCollectorService {
 
   userInfo() {
     interface ApiResponse { }
+    /*
+    Build not working if access token is in environment.ts
     let userProfileGet = this.startUrl + this.searchParam + "?access_token=" + environment.accessTokken;
+    */
+    let userProfileGet = this.startUrl + this.searchParam + "?access_token=" + this.aTokken
     return this.http.get<ApiResponse>(userProfileGet);
 
   }
 
   repoInfo() {
     interface ApiResponse { }
+    /*
+    Build not working if access token is in environment.ts
     let userReposGet = this.startUrl + this.searchParam + this.repoAdd + environment.accessTokken;
+    */
+    let userReposGet = this.startUrl + this.searchParam + this.repoAdd + this.aTokken;
     return this.http.get<ApiResponse>(userReposGet);
   }
 }
