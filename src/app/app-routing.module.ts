1400f8b351e794e64fe631f90242d8c8a8a6a58e
edumorris <eduuwachira@gmail.com>
2020-06-22 09:56:57 +0300

    Base route paths effected

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GitterComponent } from './gitter/gitter.component';
 import { ReposComponent } from './repos/repos.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 const routes: Routes = [
-  {path: 'gitter', component: GitterComponent},
-  {path: 'repos', component: ReposComponent},
+  { path: 'gitter', component: GitterComponent },
+  { path: '', component: GitterComponent },
+  { path: 'repos', component: ReposComponent },
+  { path: '**', component: NotFoundComponent },
+  { path: '', redirectTo: './gitter', pathMatch: "full" },
 ];
 
 @NgModule({
